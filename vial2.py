7bc74d54a4d583a4eeaacfb55c3b06630f39cdaf
codemuch <eric@codemuch.net>
2023-02-22 23:07:30 -0800

    tag_to_hex

--- vial2.py
+++ vial2.py
@@ -1,13 +1,35 @@
 #!/usr/bin/python
 
+import argparse
 import sys
+from rich.console import Console
 
-def to_hex(s):
-    return f"0x{''.join([hex(ord(ch)).split('x')[1] for ch in s][::-1])}"
+bn = '''[green]
+Y88b      / 888      e      888     
+ Y88b    /  888     d8b     888     ViAL ---
+  Y88b  /   888    /Y88b    888     venomous injected assembly library
+   Y888/    888   /  Y88b   888     
+    Y8/     888  /____Y88b  888     
+     Y      888 /      Y88b 888____
+[/green]'''
+
+DEFAULT_TAG = 'y0y0'
+
+def tag_to_hex(s):
+    if len(s) == 4:
+        tag = s
+    else:
+        tag = DEFAULT_TAG
+    return f"0x{''.join([hex(ord(ch)).split('x')[1] for ch in tag][::-1])}"
 
 if __name__ == '__main__':
+
+    console = Console()
+    console.print(bn)
+
     try:
         key = sys.argv[1]
-        print(to_hex(key))
+        console.log(f"Generating egghunter...")
+        console.log(f"Using key: {tag_to_hex(key)}")
     except IndexError:
         sys.exit()
