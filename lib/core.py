1f955b8c32cc822c56fad0901da05a081f8d1542
codemuch <eric@codemuch.net>
2023-10-26 11:07:40 -0700

    ipv4 and port encoding utility functions

--- lib/core.py
+++ lib/core.py
@@ -7,6 +7,18 @@ class Payload:
     
     def generate_egghunter_seh(self):
         pass
-    
+
+class Util:
     def tag_to_hex(tag):
         return f"0x{''.join([hex(ord(ch)).split('x')[1] for ch in tag][::-1])}"
+    
+    def ipv4_addr_to_hex(ip_addr):
+        li = ["{:>02}".format(hex(int(i)).split('x')[1]) for i in ip_addr.split('.')]
+        li.reverse()
+        return ''.join(li)
+
+    def port_to_hex(port_no):
+        port_dec = "{:>04}".format(hex(int(port_no)).split('x')[1])
+        port_hex = [port_dec[i:i+2] for i in range(0, len(port_dec), 2)]
+        port_hex.reverse()
+        return ''.join(port_hex)
--- vial.py
+++ vial.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 import argparse
+import os
 import sys
 from keystone import *
 from rich.console import Console
 from lib.core import Payload
+from lib.core import Util
 
 console = Console()
 
@@ -17,20 +19,13 @@ Y88b      / 888      e      888
      Y      888 /      Y88b 888____
 [/green1]'''
 
-def print_encoded_ip(ip_addr):
-    li = ["{:>02}".format(hex(int(i)).split('x')[1]) for i in ip_addr.split('.')]
-    li.reverse()
-
+def print_encoded_ipv4_addr(ip_addr):
     print("\n🧪 Encoding IPv4 address: %s" % ip_addr)
-    print("🧪 Result: 0x%s" % ''.join(li))
+    print("🧪 Result: 0x%s" % Util.ipv4_addr_to_hex(ip_addr))
 
 def print_encoded_port(port_no):
-    port_dec = "{:>04}".format(hex(int(port_no)).split('x')[1])
-    port_hex = [port_dec[i:i+2] for i in range(0, len(port_dec), 2)]
-    port_hex.reverse()
-
     print("\n🧪 Encoding port: %s" % port_no)
-    print("🧪 Result: 0x%s" % ''.join(port_hex))
+    print("🧪 Result: 0x%s" % Util.port_to_hex(port_no))
 
 def print_shellcode(code):
     ks = Ks(KS_ARCH_X86, KS_MODE_32)
@@ -46,22 +41,32 @@ def print_shellcode(code):
 def main(args):
     console.print(BANNER)
 
+    if args.encode_port:
+        port_no = args.encode_port
+        print_encoded_port(port_no)
+        sys.exit(os.EX_OK) # the port encoding option is exclusive
+
+    if args.encode_ip:
+        ip_addr = args.encode_ip
+        print_encoded_ipv4_addr(ip_addr)
+        sys.exit(os.EX_OK) # the IP encoding option is exclusive
+
     if args.egghunter:
         tag = args.tag
         op = args.egghunter[0]
 
         if len(tag) != 4:
             tag = Payload.DEFAULT_TAG
-            console.print("[yellow][WARN][/yellow] Tag must be four (4) characters!")
+            console.print('[yellow][WARN][/yellow] Tag must be four (4) characters!')
             console.print(f"[INFO] Using default tag {tag}")
 
-        console.print(f"[INFO] Generating {op} egghunter with tag {Payload.tag_to_hex(tag)} ({tag})")
+        console.print(f"[INFO] Generating {op} egghunter with tag {Util.tag_to_hex(tag)} ({tag})")
 
         if (op.lower() == 'seh'):
-            tag = Payload.tag_to_hex(tag)
+            tag = Util.tag_to_hex(tag)
             print_shellcode(Payload.generate_egghunter_seh(tag))
         elif (op.lower() == 'ntaccess'):
-            tag = Payload.tag_to_hex(tag)
+            tag = Util.tag_to_hex(tag)
             print_shellcode(Payload.generate_egghunter_ntaccess(tag))
         else:
             console.print(f"[WARN] No matching egghunter found for '{op}'!")
@@ -69,13 +74,13 @@ def main(args):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(
-        description = "creates a 32-bit Windows assembly payload"
+        description = 'creates a 32-bit Windows assembly payload'
     )
 
     exclusive_group = parser.add_mutually_exclusive_group()
     exclusive_group.add_argument(
         '--egghunter',
-        help = "generate a 32-bit Windows SEH or NtAccessCheckAndAuditAlarm egghunter",
+        help = 'generate a 32-bit Windows SEH or NtAccessCheckAndAuditAlarm egghunter',
         action = 'store',
         type = str,
         nargs = 1,
@@ -83,7 +88,7 @@ if __name__ == '__main__':
     )
     parser.add_argument(
         '--payload',
-        help = "generate a 32-bit Windows bind or reverse shell payload",
+        help = 'generate a 32-bit Windows bind or reverse shell payload',
         action = 'store',
         type = str,
         nargs = 1,
@@ -91,7 +96,7 @@ if __name__ == '__main__':
     )
     exclusive_group.add_argument(
         '--list',
-        help = "list available payloads",
+        help = 'list available payloads',
         action = 'store_true',
     )
 
@@ -103,9 +108,23 @@ if __name__ == '__main__':
         default = Payload.DEFAULT_TAG
     )
 
+    parser.add_argument(
+        '--encode-ip',
+        help = f"encode an IPv4 address",
+        action = 'store',
+        type = str,
+    )
+
+    parser.add_argument(
+        '--encode-port',
+        help = f"encode a port number",
+        action = 'store',
+        type = str,
+    )
+
     if len(sys.argv) > 1:
         args = parser.parse_args()
         main(args)
     else:
         parser.print_help()
-        sys.exit()
+        sys.exit(os.EX_OK)
